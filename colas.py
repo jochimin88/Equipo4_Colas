dcb4ded6e403ad0174d7872e50c79fe9a8e0041b
Jochimin <54875738+jochimin88@users.noreply.github.com>
2021-02-06 16:49:50 -0400

    Update colas.py

--- colas.py
+++ colas.py
@@ -1,1 +1,23 @@
 
+# Creamos la variable mi_cola donde almacenara los datos.
+
+# creamos la lista vacia nombrada mi_cola
+mi_cola = []
+
+# Imprimimos en pantalla
+print(mi_cola)
+
+# En la terminal debemos ver los siguiente resultado:
+
+#  []
+
+# Podemos agregar los elementos directamente a la lista
+
+mi_cola = [1,2,3,4,'Grupo4']
+
+# Verificamos si nuestra cola contiene elementos
+
+if not mi_cola:
+    print("Cola Vacia!")
+else:
+    print("Cola con elementos", mi_cola)
