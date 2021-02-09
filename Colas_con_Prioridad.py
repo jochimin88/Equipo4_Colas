a2b24f85a1dd2911d08f1d5e1c9d7a06ec72b0b2
Mijarex <61257669+Mijarex@users.noreply.github.com>
2021-02-08 22:16:56 -0500

    Update Colas_con_Prioridad.py

--- Colas_con_Prioridad.py
+++ Colas_con_Prioridad.py
@@ -1,1 +1,24 @@
 
+#cometarios #
+
+
+from queue import PriorityQueue 
+
+class Curso(object):
+    
+    def __init__(self, prioridad, nombre):
+        self.prioridad = prioridad
+        self.nombre = nombre
+    
+    def __lt__(self, otro):
+        return self.prioridad > otro.prioridad
+    
+
+cursos = PriorityQueue()
+cursos.put(Curso(3, 'Python'))
+cursos.put(Curso(10, 'C++'))
+cursos.put(Curso(1, 'java'))
+
+while not cursos.empty():
+    c = cursos.get()
+    print(c.prioridad,c.nombre)
