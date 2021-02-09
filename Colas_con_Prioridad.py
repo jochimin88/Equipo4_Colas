212229571b808f2d028c0a559761fa392f6c6827
Mijarex <61257669+Mijarex@users.noreply.github.com>
2021-02-09 17:02:29 -0500

    Update Colas_con_Prioridad.py

--- Colas_con_Prioridad.py
+++ Colas_con_Prioridad.py
@@ -1,24 +1,29 @@
-
 #cometarios #
+#MODULO queue El módulo estándar Queue o queue (Python 2 o 3, respectivamente) permite crear y trabajar con colas de manera sencilla. 
+#Es generalmente utilizado en programas multi-hilo, ya que provee una forma de intercambiar información entre threads de manera segura.
 
-
+#Priority Queue (cola de prioridad): el ítem de menor valor es el primero en ser retornado.
 from queue import PriorityQueue 
 
-class Curso():
-    
-    def __init__(self, prioridad, nombre):
-        self.prioridad = prioridad
-        self.nombre = nombre
-    
-    def __lt__(self, otro):
-        return self.prioridad > otro.prioridad
-    
 
-cursos = PriorityQueue()
-cursos.put(Curso(3, 'Python'))
-cursos.put(Curso(10, 'C++'))
-cursos.put(Curso(1, 'java'))
+class Curso():#Creando una clase llamada curso#
+    
+    def __init__(self, prioridad, nombre):# Aqui creamos un iniciador colocamos a self con los siguientes parametros#
+        self.prioridad = prioridad  #  Tenemos una atributo llamado prioridad y le agsinamos el contenido del parametro prioridad# 
+        self.nombre = nombre #  Tenemos una atributo llamado nombre y le agsinamos el contenido del parametro nombre#
+    
+    def __lt__(self, otro):#metodo lestar como menor que ,Siendo necesario devolver el objeto resultante de la operación o el resultado en el caso de ser una operación lógica#
+        return self.prioridad < otro.prioridad # retornamos la siguiente expresion prioridad es menor a la prioridad del otro elemento .
+    
+#comentarios#
+# el metodo put inserta item en la cola ,En las colas de prioridad los ítems generalmente se insertan como una tupla#
+#indicando el número en el primer lugar y los datos en el segundo. Por ejemplo#
+    
+cursos = PriorityQueue() # creamos una variables de nombre curso instanciando la libreria PriorityQueue#
+cursos.put(Curso(3, 'Python'))#agregando las colas con el metodo put#
+cursos.put(Curso(10, 'C++'))#agregando las colas con el metodo put#
+cursos.put(Curso(1, 'java'))#agregando las colas con el metodo put#
 
-while not cursos.empty():
+while not cursos.empty():# haciendo un ciclo preguntando si esta vacio#
     c = cursos.get()
-    print(c.prioridad,c.nombre)
+    print(c.prioridad,c.nombre)# Imprimos la prioridad y el nombre del curso#
