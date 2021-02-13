61ae621e211037581b21c8716cbc740e524e0fb4
Jochimin <54875738+jochimin88@users.noreply.github.com>
2021-02-13 10:34:54 -0400

    Update colas.py
    dunder

--- colas.py
+++ colas.py
@@ -6,34 +6,14 @@ mi_cola = []
 
 # Imprimimos en pantalla
 print(mi_cola)
-    
-mi_cola[2]
-# El Resultado sera "Josue", porque los arreglos comienzan en cero.
 
-mi_cola[5]
-# El Resultado sera "Jochimin", porque los arreglos comienzan en cero.
-# En la terminal debemos ver los siguiente resultado:
+#Resultado:
+#  [], Una lista vacia
 
-#  []
-
-# Podemos agregar los elementos directamente a la lista
-
-mi_cola = [1,2,3,4,'Grupo4']
-
-# Verificamos si nuestra cola contiene elementos
-
-if not mi_cola:
-    print("Cola Vacia!")
-else:
-    print("Cola con elementos", mi_cola)
-    
 # Anadir elementos a la cola.
 
 # Para anadir elementos a una cola necesitamos el metodo append()
 # Este metodo agrega un elemento al final de una lista.
-
-# aqui agragamos el elemento numero 1 a nuestra cola
-# y asi sucecivamente vamos agregamos.
 
 mi_cola.append("Joselito")
 mi_cola.append("Chantal")
@@ -47,17 +27,29 @@ mi_cola.append("Polanco")
 
 print(mi_cola)
 # Resultado sera:
-['Joselito', 'Chantal', 'Josue', 'Mijarex', 'Francelys', 'Jochimin', 'Polanco']
+# ['Joselito', 'Chantal', 'Josue', 'Mijarex', 'Francelys', 'Jochimin', 'Polanco']
 
+
+# Tambien podemos agregar los elementos directamente a la lista
+
+#mi_cola = [1,2,3,4,'Grupo4']
+
+# Verificamos si nuestra cola contiene elementos
+
+if not mi_cola:
+    print("Cola Vacia!")
+else:
+    print("Cola con elementos", mi_cola)
+    
 
 # Para Leer un elemento en una cola.
 # en ese caso utilizaremos los index del arreglo
 # de nuestra lista, para acceder a dicho indice utilizamos
 # los corchetes y dentro de estos assignamos el numero del indice que queremos.
 
-mi_cola[0] # Este indice muestra la posicion en la lista
-mi_cola[3] # El Resultado sera "Joselito", porque los arreglos comienzan en cero.
-# El Resultado sera "Mijarex", porque los arreglos comienzan en cero.
+# Este indice muestra la posicion en la lista
+mi_cola[0] # El resultado es: "Joselito"
+mi_cola[3] # El Resultado sera "Josue", porque los arreglos comienzan en cero.
 
 
 # Para realizar algo mas dinamico con las listas
@@ -72,12 +64,12 @@ class Cola:
     # Inicializamos nuestra clase y creamos una variable
     # llamada mi mi_cola que es una lista vacia.
     
-    def _init_(self):
+    def __init__(self):
         self.mi_cola = []
 
 
     # Verificamos si la lista esta vacia
-    # En caso de no estar vacia nos devuelve los elementos que 
+    # En caso de no estar vacia nos devuelve los elementos que `
     # los componen.
 
     def esta_vacia(self):
